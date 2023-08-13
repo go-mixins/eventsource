96499ff5a8d98030f625a0effb8f91d6765c7c8e
Andrew Rodionoff <andviro@gmail.com>
2023-08-13 17:08:36 +0300

    [example] adapted to new process ideology

--- example/main.go
+++ example/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"errors"
-	"fmt"
 	"log/slog"
 	"os"
 	"time"
@@ -14,9 +13,14 @@ import (
 
 	g "github.com/go-mixins/gorm/v4"
 	"github.com/go-mixins/log/v2"
+	"github.com/go-mixins/pubsub"
+	_ "github.com/go-mixins/pubsub/gocloud"
+	_ "gocloud.dev/pubsub/mempubsub"
 
 	"github.com/carlmjohnson/versioninfo"
+
 	"github.com/go-mixins/eventsource"
+	"github.com/go-mixins/eventsource/driver"
 	"github.com/go-mixins/eventsource/driver/gorm"
 )
 
@@ -47,33 +51,44 @@ func main() {
 	if err := backend.Connect(true); err != nil {
 		panic(err)
 	}
-	es := eventsource.NewService[Patient](eventsource.NewRepository[Patient](backend.WithDebug()))
+	topicURL := "gocloud+json+mem://patient-events"
+	topic, err := pubsub.OpenTopic[driver.Event[string]](ctx, topicURL)
+	if err != nil {
+		panic(err)
+	}
+	defer topic.Shutdown(ctx)
+	repository := eventsource.NewRepository[Patient](backend, topic)
+	es := eventsource.NewService(repository)
 	es.Repository.RegisterEvents(PatientCreated{}, PatientTransferred{}, PatientDischarged{})
-	es.Repository.Subscribe(func(n eventsource.Notification[Patient, string]) {
-		slog.InfoContext(ctx, "signaled", "event", fmt.Sprintf("%T: %+v", n.Payload, n.Payload), "aggregate", n.AggregateID)
-	})
-	es.Handle(
-		eventsource.Rule(func(ctx context.Context, t Patient, e PatientCreated) ([]eventsource.Command[Patient], error) {
+	sub, err := pubsub.Handle(ctx, topicURL, es.ProcessNotification)
+	if err != nil {
+		panic(err)
+	}
+	defer sub.Shutdown(ctx)
+	if err := es.Handle(
+		eventsource.Rule(func(ctx context.Context, t Patient, e *PatientCreated) ([]eventsource.Command[Patient], error) {
 			return []eventsource.Command[Patient]{
 				Transfer{NewWard: t.ward + 1},
 			}, nil
 		}),
-		eventsource.Rule(func(ctx context.Context, t Patient, e PatientTransferred) ([]eventsource.Command[Patient], error) {
+		eventsource.Rule(func(ctx context.Context, t Patient, e *PatientTransferred) ([]eventsource.Command[Patient], error) {
 			return []eventsource.Command[Patient]{
 				Discharge{},
 			}, nil
 		}),
-	)
+	); err != nil {
+		panic(err)
+	}
 	id := xid.New().String()
 	if err := es.Execute(ctx, id, Create{Ward: 1, Name: "Vasya", Age: 21}); err != nil {
-		slog.ErrorContext(ctx, "execution failed", "error", err)
+		slog.ErrorContext(ctx, "execution failed", "aggregate_id", id, "error", err)
 		return
 	}
 	time.Sleep(time.Second) // Wait for process to complete
 	if err := es.Execute(ctx, id, Transfer{NewWard: 3}); errors.Is(err, eventsource.ErrCommandAborted) {
-		slog.WarnContext(ctx, "not tranferring discharged patient")
+		slog.WarnContext(ctx, "not tranferring discharged patient", "aggregate_id", id)
 	} else if err != nil {
-		slog.ErrorContext(ctx, "execution failed", "error", err)
+		slog.ErrorContext(ctx, "execution failed", "aggregate_id", id, "error", err)
 	}
 	time.Sleep(time.Second)
 }
